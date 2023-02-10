c49420c530903d3517f9a2338df22b46ac5282fd
VenusPR <albertjong112@gmail.com>
2023-02-10 10:32:44 +0900

    REST API 作り直し１

--- src/app/Http/Controllers/Api/AccountController.php
+++ src/app/Http/Controllers/Api/AccountController.php
@@ -12,64 +12,40 @@ use App\Models\User;
 class AccountController extends Controller
 {
 
-    public function banks(Request $request)
-    {
-        $user_id = 1;
-        $banks = Bank::where('user_id', $user_id)->get();
-        return $banks;
-    }
+  /**
+   * Test
+   */
+  // public function xgetBanks(Request $request)
+  // {
+  //   $user_id = 1;
+  //   $banks = Bank::where('user_id', $user_id)->get();
+  //   return $banks;
+  // }
 
   /**
    * 個人のすべての口座を参照
    */
-    public function allBanks(Request $request)
+  public function getBanks(Request $request)
   {
-      $user_id = 1;
-      $banks = Bank::where('user_id', $user_id)->get();
-      return response()->json($banks,200,[],JSON_UNESCAPED_UNICODE);
+    $user_id = 1;
+    $banks = Bank::where('user_id', $user_id)->get();
+    return $banks;
   }
 
-  /**
-   * 個人のすべての口座を参照
-   */
-  // public function xgetBanks(Request $request, int $id)
-  // {
-  //    $user_id = 1;
-  //    if(empty($id)){//idがない場合はすべての口座を返す
-  //     if (Bank::where('user_id', $user_id)->exists()){
-  //       $banks = Bank::where('user_id', $user_id)->get();
-  //       return response()->json($banks,200,[],JSON_UNESCAPED_UNICODE);
-  //     }else {
-  //         return response()->json([
-  //             'message' => 'this user is bank not found'
-  //         ], 404);
-  //     }
-  //    }elseif(!empty($id)){//idがある場合は一つの口座を返す
-  //     if (Bank::where('user_id', $user_id)->where('id', $id)->exists()){
-  //       $bank = Bank::find($id);
-  //     }else {
-  //       return response()->json([
-  //           'message' => 'this user is bank not found(one)'
-  //       ], 404);
-  //   }
-  //     return response()->json($bank,200,[],JSON_UNESCAPED_UNICODE);
-  //    }
-     
-  // }
 
   /**
    * 個人の一つ口座を参照
    */
-  public function oneBank(Request $request, int $id)
+  public function postBanks(Request $request, int $id)
   {
-      if (Bank::where('id', $id)->exists()){
-        $bank = Bank::find($id);
-      }else {
-        return response()->json([
-            'message' => 'bank not found'
-        ], 404);
+    if (Bank::where('id', $id)->exists()) {
+      $bank = Bank::find($id);
+    } else {
+      return response()->json([
+        'message' => 'bank not found'
+      ], 404);
     }
-      return response()->json($bank,200,[],JSON_UNESCAPED_UNICODE);
+    return $bank;
   }
 
 
@@ -82,37 +58,36 @@ class AccountController extends Controller
    * account_number
    * account_name
    */
-    public function updateBank(Request $request, int $id)
+  public function putBanks(Request $request, int $id)
   {
     $user_id = 1;
-    if (Bank::where('id', $id)->exists())
-        {
-            $bank = Bank::find($id);
-            if (!empty($request->bank_name)) {
-                $bank->bank_name = $request->bank_name;
-            }
-            if (!empty($request->branch_name)) {
-                $bank->branch_name = $request->branch_name;
-            }
-            if (isset($request->account_type)) {
-                $bank->account_type = $request->account_type;
-            }
-            if (isset($request->account_number)) {
-              $bank->account_number = $request->account_number;
-            }
-              if (isset($request->account_name)) {
-                $bank->account_name = $request->account_name;
-            }
-            $bank->save();
+    if (Bank::where('id', $id)->exists()) {
+      $bank = Bank::find($id);
+      if (!empty($request->bank_name)) {
+        $bank->bank_name = $request->bank_name;
+      }
+      if (!empty($request->branch_name)) {
+        $bank->branch_name = $request->branch_name;
+      }
+      if (isset($request->account_type)) {
+        $bank->account_type = $request->account_type;
+      }
+      if (isset($request->account_number)) {
+        $bank->account_number = $request->account_number;
+      }
+      if (isset($request->account_name)) {
+        $bank->account_name = $request->account_name;
+      }
+      $bank->save();
 
-            return response()->json([
-                'message' => 'updated bank info'
-            ], 201);
-        } else {
-            return response()->json([
-                'message' => 'bank not found'
-            ], 404);
-        }
+      return response()->json([
+        'message' => 'updated bank info'
+      ], 201);
+    } else {
+      return response()->json([
+        'message' => 'bank not found'
+      ], 404);
+    }
   }
 
   /**
@@ -124,41 +99,40 @@ class AccountController extends Controller
    * account_number
    * account_name
    */
-  public function createBank(Request $request)
-    {
-      $user_id = 1;
-      $bank = new Bank();
-      $bank->user_id = $user_id;
-      $bank->bank_name = $request->bank_name;
-      $bank->branch_name = $request->branch_name;
-      $bank->account_type = $request->account_type;
-      $bank->account_number = $request->account_number;
-      $bank->account_name = $request->account_name;
-      $bank->save();
+  public function xpostBanks(Request $request)
+  {
+    $user_id = 1;
+    $bank = new Bank();
+    $bank->user_id = $user_id;
+    $bank->bank_name = $request->bank_name;
+    $bank->branch_name = $request->branch_name;
+    $bank->account_type = $request->account_type;
+    $bank->account_number = $request->account_number;
+    $bank->account_name = $request->account_name;
+    $bank->save();
+
+    return response()->json([
+      'message' => 'created bank'
+    ], 201);
+  }
+
+  /**
+   * 個人の口座の削除
+   */
+  public function destoryBank(Request $request, int $id)
+  {
+    // return ['test' => $id];
+    if (Bank::where('id', $id)->exists()) {
+      $bank = Bank::find($id);
+      $bank->delete();
 
       return response()->json([
-          'message' => 'created bank'
-      ], 201);
+        'message' => 'delete bank'
+      ], 202);
+    } else {
+      return response()->json([
+        'message' => 'bank not found'
+      ], 404);
     }
-
-    /**
-     * 個人の口座の削除
-     */
-    public function destoryBank(Request $request, int $id)
-    {
-      // return ['test' => $id];
-        if (Bank::where('id', $id)->exists())
-        {
-            $bank = Bank::find($id);
-            $bank->delete();
-
-            return response()->json([
-                'message' => 'delete bank'
-            ], 202);
-        } else {
-            return response()->json([
-                'message' => 'bank not found'
-            ], 404);
-        }
-    }
+  }
 }
