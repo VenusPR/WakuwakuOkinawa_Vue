3665d34d32b38a421cfa80d0a6d57f2930baf803
VenusPR <albertjong112@gmail.com>
2023-02-14 18:20:07 +0900

    URLの変更「api/profiles/me/image」しました。

--- src/routes/api.php
+++ src/routes/api.php
@@ -29,6 +29,7 @@ Route::auto('/debug', DebugController::class); // デバッグ用
 
 // Route::auto('/accounts', AccountController::class);
 Route::apiResource('/banks', BanksController::class);
+Route::get('/profiles/me/image', [ImagesController::class, 'index']);
+Route::post('/profiles/me/image', [ImagesController::class, 'store']);
 Route::apiResource('/profiles/me', ProfilesController::class);
 Route::apiResource('/prefectures', PrefecturesController::class);
-Route::apiResource('/images/me', ImagesController::class);
