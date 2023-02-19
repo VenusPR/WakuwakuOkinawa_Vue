8ff588fc7dc3e71bc386fcd3102886779a76697a
VenusPR <albertjong112@gmail.com>
2023-02-19 09:21:02 +0900

    kidsのfrontの作り始め

--- src/resources/js/src/router/index.js
+++ src/resources/js/src/router/index.js
@@ -14,6 +14,7 @@ const routes = [
     { path: '/', name: 'Home', component: Home },
     { path: '/profile', name: 'Profile', component: () => import('../views/profile/index.vue'), },
     { path: '/profile/bank', name: 'Bank', component: () => import('../views/profile/bank.vue'), },
+    { path: '/profile/kids', name: 'Kid', component: () => import('../views/profile/kids.vue'), },
 
     // ------- 以下は sample --------------------------------
 
